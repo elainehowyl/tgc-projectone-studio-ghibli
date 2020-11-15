d93324308d6bce2f887b3e9d9359430dcaa035b4
Your Name <you@example.com>
2020-11-16 01:21:45 +0800

    Finalised index page, roughly drafted homepage

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-//script for index.html
-setTimeout(function(){
-    let loadingBar = document.querySelector('#loading-bar');
-    loadingBar.style.visibility='hidden';
-},10000);
-
 setTimeout(function(){
     let loadingBarContainer = document.querySelector('#loading-bar-container');
-    loadingBarContainer.style.opacity='0';
-},10000);
+    loadingBarContainer.style.visibility='hidden'; 
+},8000);
+
 
 setTimeout(function(){
     let enterButton = document.querySelector('#enter');
     enterButton.style.display='block';
-},11000);
+},9000);
