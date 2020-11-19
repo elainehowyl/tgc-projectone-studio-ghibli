1655cca3874b8177510fed04116c8528a255cfc7
Your Name <you@example.com>
2020-11-20 02:05:51 +0800

    moved loading page to another tab, created pricing page

--- explorepage.js
+++ explorepage.js
@@ -1,4 +1,3 @@
-// for index.html
 setTimeout(function(){
     document.querySelector('#loading-bar-container').style.visibility='hidden';
 },8000);
@@ -7,4 +6,3 @@ setTimeout(function(){
 setTimeout(function(){
     document.querySelector('#enter').style.display='block';
 },9000);
-
