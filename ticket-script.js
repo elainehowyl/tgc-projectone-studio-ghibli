4c07a428be1f04d9a3586696810e485f018ff53b
Your Name <you@example.com>
2020-11-22 03:29:57 +0800

    changed some styling and added half script on ticket page

--- ticket-script.js
+++ ticket-script.js
@@ -19,3 +19,49 @@ document.querySelector('#back2').addEventListener('click',function(){
     layerTwo.style.display='block';
     layerThree.style.display='none';
 })
+
+let priceList = {
+    'individual': 23,
+    'double': 42,
+    'family': 85,
+}
+
+document.querySelector('#submit').addEventListener('click', function(){
+    //calculate total price
+    let allChoices = document.querySelectorAll('.ticket-choice');
+    let selectedChoice = [];
+    for (let choice of allChoices){
+      if(choice.checked){
+        selectedChoice.push(choice.value)
+      }
+    }
+    let individualQuantity = document.querySelector('#individual-select').value;
+    let doubleQuantity = document.querySelector('#double-select').value;
+    let familyQuantity = document.querySelector('#family-select').value;
+
+    let total = 0;
+    for(eachChoice of selectedChoice){
+      if(eachChoice == 'individual'){
+        total += priceList[eachChoice]*individualQuantity;
+      }
+      if(eachChoice == 'double'){
+        total += priceList[eachChoice]*doubleQuantity;
+      }
+      if(eachChoice == 'family'){
+        total += priceList[eachChoice]*familyQuantity;
+      }
+    }
+    console.log("Total cost: $", total)
+    
+    //return selected date and time
+    let dateTimeChoice = document.querySelectorAll('.date-choice')
+    for(let dateChoice of dateTimeChoice){
+        if(dateChoice.checked){
+            console.log("Date selected: ", dateChoice.value)
+        }
+    }
+    let selectedTime = document.querySelector('#time-selection').value
+    console.log("Time selected: ", selectedTime)
+})
+
+
