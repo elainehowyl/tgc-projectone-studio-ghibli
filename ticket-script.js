2f0f7a8b9be0cb65f19722f1348df67603a8f337
Your Name <you@example.com>
2020-11-23 18:22:37 +0800

    completed form validation, added gallery page

--- ticket-script.js
+++ ticket-script.js
@@ -26,8 +26,9 @@ let priceList = {
     'family': 85,
 }
 
-document.querySelector('#submit').addEventListener('click', function(){
-    //calculate total price
+document.querySelector('#submit').addEventListener('click', function(event){
+    event.preventDefault();
+    //CALCULATE TOTAL PRICE
     let allChoices = document.querySelectorAll('.ticket-choice');
     let selectedChoice = [];
     for (let choice of allChoices){
@@ -53,7 +54,7 @@ document.querySelector('#submit').addEventListener('click', function(){
     }
     console.log("Total cost: $", total)
     
-    //return selected date and time
+    //RETURN SELECTED TIME AND DATE
     let dateTimeChoice = document.querySelectorAll('.date-choice')
     for(let dateChoice of dateTimeChoice){
         if(dateChoice.checked){
@@ -62,6 +63,80 @@ document.querySelector('#submit').addEventListener('click', function(){
     }
     let selectedTime = document.querySelector('#time-selection').value
     console.log("Time selected: ", selectedTime)
+
+    //CARD INPUT VALIDATION
+    
+    //initialise validation status
+    let cardNumberIsInvalid = false;
+    let cardExpiryDateIsInvalid = false;
+    let cardCvvIsInvalid = false;
+    let nameIsInvalid = false;
+    let phoneNumIsInvalid = false;
+    let emailIsInvalid = false;
+
+    //set validation condition
+    let cardNumber = document.querySelector('#card-number').value
+    if (cardNumber.length != 19){
+      cardNumberIsInvalid = true;
+      alert("Invalid card number! Please re-enter!")
+    }
+    else{
+      console.log("Card Number: ", cardNumber)
+    }
+    let cardExpiryDate = document.querySelector('#card-date').value
+    if (cardExpiryDate.includes('/') == false){
+      cardExpiryDateIsInvalid = true;
+      alert("Expiry Date is invalid! Please re-enter!")
+    }
+    else{
+      console.log("Card Expiry Date: ", cardExpiryDate)
+    }
+
+    let cardCvv = document.querySelector('#card-cvv').value
+    if (cardCvv.length != 3){
+      cardCvvIsInvalid = true;
+      alert("Card CVV is invalid. Please re-enter!")
+    }
+    else{
+      console.log("Card CVV: ", cardCvv)
+    }
+    
+    let nameOnCard = document.querySelector('#card-name').value
+    if (nameOnCard.length == 0){
+      nameIsInvalid = true;
+      alert("Name is empty. Please enter name on card.")
+    }
+    else{
+      console.log("Name: ", nameOnCard)
+    }
+
+    let phoneNum = document.querySelector('#phone-number').value
+    if (phoneNum.length != 8){
+      phoneNumIsInvalid = true;
+      alert("Invalid phone number. Please re-enter a valid number.")
+    }
+    else{
+      console.log("Phone Number: ", phoneNum)
+    }
+
+    let email = document.querySelector('#email').value
+    if (email.includes('@') == false){
+      emailIsInvalid = true;
+      alert("Invalid email. Please re-enter a valid email.")
+    }
+    else{
+      console.log("Email: ", email)
+    }
+
+    let agreement = document.querySelector('#agreement').value
+    if(agreement.checked){
+      console.log("Customer wish to be informed.")
+    }
+    else{
+      console.log("No, customer do not wish to be informed.")
+    }
+
+    if(!cardExpiryDateIsInvalid && !cardCvvIsInvalid && !nameIsInvalid && !phoneNumIsInvalid && !emailIsInvalid){
+      form.submit()
+    }
 })
-
-
